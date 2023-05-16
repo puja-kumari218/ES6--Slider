72bdde07469090bf5de0dfeb4d02f8bb99f4f71a
puja kumari <pkumari41038@gmail.com>
2023-05-16 12:39:54 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,29 @@
+const people = [
+  {
+    img: 'https://media.istockphoto.com/id/1417044604/photo/thoughtful-anonymous-woman-in-store.jpg?s=170667a&w=0&k=20&c=OY67AvJsG1-PZb5QtfcApFIUM3ou8KP2wVhwvB6dOeA=',
+    name: 'peter doe',
+    job: 'product manager',
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Et quasi 
+    quos blanditiis quo odit a omnis soluta fugit, esse nemo quis rerum!
+     Soluta sapiente nesciunt excepturi eos. Velit, ipsum repellendus.`,
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1613293241506-523dcf0cb1bb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    name: 'susan doe',
+    job: 'developer',
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Et quasi 
+    quos blanditiis quo odit a omnis soluta fugit, esse nemo quis rerum!
+     Soluta sapiente nesciunt excepturi eos. Velit, ipsum repellendus.`,
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1537511446984-935f663eb1f4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fHBlb3BlbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
+    name: 'emma doe',
+    job: 'designer',
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Et quasi 
+    quos blanditiis quo odit a omnis soluta fugit, esse nemo quis rerum!
+     Soluta sapiente nesciunt excepturi eos. Velit, ipsum repellendus.`,
+
+  },
+];
+
+export default people;
